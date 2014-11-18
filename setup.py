4b92946814f9ce8e6feedcf09ff30427322faaa5
Quentin Kaiser <kaiserquentin@gmail.com>
2014-11-18 14:41:17 +0100

    Setuptools deployment.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+VERSION = __import__("pynessus").__version__
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Security',
+    'Operating System :: OS Independent',
+]
+setup(
+    name="pynessus",
+    description="Nessus python client.",
+    version=VERSION,
+    author="Quentin Kaiser",
+    author_email="kaiserquentin@gmail.com",
+    url="https://github.com/qkaiser/pynessus",
+    download_url="https://github.com/qkaiser/pynessus/releases/latest",
+    package_dir={'pynessus': 'pynessus'},
+    packages=['pynessus'],
+    classifiers=CLASSIFIERS,
+)
