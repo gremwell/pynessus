2417fd9da9ae78fab0574f99d6244fbb70a4802c
Quentin Kaiser <kaiserquentin@gmail.com>
2014-11-21 17:49:23 +0100

    Update version.

--- pynessus/__init__.py
+++ pynessus/__init__.py
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-__version__ = "0.2.0"
+__version__ = "0.2.2"
 from nessus import Nessus, NessusAPIError
 from models.policy import Policy
 from models.report import Report
