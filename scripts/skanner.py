5cfdb905788665d39dc7fd7aea192200986abcfe
Quentin Kaiser <kaiserquentin@gmail.com>
2016-03-23 14:57:02 +0100

    Put skanner.py script in /usr/local/bin on install.

--- scripts/skanner.py
+++ scripts/skanner.py
@@ -1,11 +1,92 @@
+#!/usr/bin/env python
 import time
 import sys
-from optparse import OptionParser
 import os
+import ConfigParser
+from datetime import datetime
+from optparse import OptionParser
 
-from framework import Framework, Colors
 from pynessus import Nessus
 
+class Colors(object):
+    N = '\033[m'  # native
+    R = '\033[31m'  # red
+    G = '\033[32m'  # green
+    O = '\033[33m'  # orange
+    B = '\033[34m'  # blue
+
+
+class Framework(object):
+
+    def __init__(self, configfile):
+        self.config = ConfigParser.ConfigParser()
+        self.config.readfp(open(configfile))
+
+        loglevels = {'debug': logging.DEBUG,
+                     'info': logging.INFO,
+                     'warning': logging.WARNING,
+                     'error': logging.ERROR,
+                     'critical': logging.CRITICAL}
+        self.logformat = "%s %8s %s"
+        # Core settings
+        self.logfile = self.config.get('core', 'logfile')
+        self.loglevel = loglevels[self.config.get('core', 'loglevel')]
+
+        # Setup some basic logging.
+        self.logger = logging.getLogger('Nessus')
+        self.logger.setLevel(self.loglevel)
+        self.logger.addHandler(logging.FileHandler(self.logfile))
+
+        self.logger.debug("CONF configfile = %s" % configfile)
+        self.logger.debug("Logger initiated; Logfile: %s, Loglevel: %s" % (self.logfile, self.loglevel))
+
+    def debug(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Debug message to be written to the log.
+        """
+        self.logger.debug(self.logformat % (datetime.now(), 'DEBUG', msg))
+
+    def alert(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Alert message to be written to the log.
+        """
+        print('%s[*]%s %s' % (Colors.G, Colors.N, msg))
+        self.logger.info(self.logformat % (datetime.now(), 'INFO', msg))
+
+    def info(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Info message to be written to the log.
+        """
+        print('%s[*]%s %s' % (Colors.B, Colors.N, msg))
+        self.logger.info(self.logformat % (datetime.now(), 'INFO', msg))
+
+    def warning(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Warning message to be written to the log.
+        """
+        print('%s[#] %s%s' % (Colors.O, msg, Colors.N))
+        self.logger.warning(self.logformat % (datetime.now(), 'WARNING', msg))
+
+    def error(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Error message to be written to the log.
+        """
+        print('%s[!] %s%s' % (Colors.R, msg, Colors.N))
+        self.logger.info(self.logformat % (datetime.now(), 'ERROR', msg))
+        sys.exit(-1)
+
+    def critical(self, msg):
+        """
+        @type   msg:    string
+        @param  msg:    Critical message to be written to the log.
+        """
+        print('%s[!] %s%s' % (Colors.R, msg, Colors.N))
+        self.logger.critical(self.logformat % (datetime.now(), 'CRITICAL', msg))
 
 class Skanner(Framework):
 
@@ -119,5 +200,8 @@ if __name__ == "__main__":
     parser.add_option("-s", dest='scan_uuid', help="scan uuid to hook")
     (options, args) = parser.parse_args()
 
-    skanner = Skanner(options.configfile)
-    skanner.run(options)
+    if options.configfile is None:
+        parser.print_help()
+    else:
+        skanner = Skanner(options.configfile)
+        skanner.run(options)
